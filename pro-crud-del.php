ec28aef0848112ae48cc12409675746f6994ae90
abenezerabrahamiftar <9028148@student.zadkine.nl>
2026-07-08 16:54:46 +0200

    upd

--- pro-crud-del.php
+++ pro-crud-del.php
@@ -1,1 +1,23 @@
-<?php include "underconstruct.php"; ?>
+<?php
+require 'dbconnect.php';
+
+$id = $_GET['id'];
+
+// Check of het product bestaat
+$sql = "SELECT * FROM product WHERE id = :id";
+$stmt = $db->prepare($sql);
+$stmt->execute([':id' => $id]);
+$product = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$product) {
+    die("Product met ID $id bestaat niet.");
+}
+
+// Verwijder product
+$sql = "DELETE FROM product WHERE id = :id";
+$stmt = $db->prepare($sql);
+$stmt->execute([':id' => $id]);
+
+// Terug naar overzicht
+header("Location: pro-crud-get.php");
+exit;
